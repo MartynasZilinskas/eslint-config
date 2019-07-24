87cf5f050b420cc0fa377a4b7ea93911c1561607
Martynas Žilinskas <martiogalaltu@gmail.com>
2019-07-24 23:16:44 +0300

    Init.

---
+++ react.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: ["@projectstorm"],
+    plugins: ["react", "react-hooks"],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    rules: {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/jsx-key": "error",
+        "react/jsx-pascal-case": "error",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error",
+        "react/button-has-type": "error",
+        "react/no-string-refs": "error"
+    }
+};
